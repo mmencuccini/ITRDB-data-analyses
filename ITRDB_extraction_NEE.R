5ab1bfeb09a132bffaff671a9bca56cdf97d145a
Maurizio Mencuccini <m.mencuccini@ed.ac.uk>
2020-08-10 12:28:40 +0200

    Update ITRDB_extraction_NEE.R

--- ITRDB_extraction_NEE.R
+++ ITRDB_extraction_NEE.R
@@ -1,5 +1,4 @@
-## procedure to extract missing files from Shilong Piao list of 332 sites in Li_etal_sites df
-## it creates an INFOSET_NEW df containing only the data from the Li et al sites
+## procedure to create an INFOSET_NEW df containing only the data from the Li et al sites
 ## and a new data frame (all series), containing all new and old sites (3889 series).
 
 ## libraries ________________________________####
@@ -28,9 +27,6 @@ simpleCap <- function(x) {
 
 ## Reading Zhao et al 2019 rwl folder into crn format ___________________________#####
 
-
-
-wd <- setwd("D:/Dropbox/My Documents/science/projects/grants/China Tong Zheng/Zhao ITRDB dataset/AppendixS1/Cleaned datasets")
 wd <- normalizePath(wd, winslash = "/", mustWork = NA)
 setwd(wd)
 
@@ -45,11 +41,7 @@ for(j in 1:length(list_dirs)) {
   for(i in 1:length(list_files)) {
     file <- read.rwl(fname = list_files[i], format = 'tucson')
     if(!all(unlist(lapply(file, function(x) any(is.na(x) | any(x==0)))))) { # is not all NA and not all 0, then
-      
-      # this option employed the Li et al approach of spline detrending
-      # dtrdn_file <- detrend(file, make.plot = FALSE, method = 'Spline', f = 0.5, nyrs = 30, difference = FALSE)
-      
-      # this option employed the Gazol et al 2016 approach of Neg Exp detrending
+          
       dtrdn_file <- detrend(file, make.plot = FALSE, method = 'ModNegExp', difference = FALSE)
       name <- strsplit(list_files[i], split = '.rwl')
       name <- paste(name, '.crn', sep='')
@@ -70,8 +62,6 @@ for(j in 1:length(list_dirs)) {
 ## Reading new crn files from folder ______________________________####
 
 setwd('..')
-setwd("D:/Dropbox/My Documents/science/projects/grants/China Tong Zheng/Zhao ITRDB dataset/AppendixS1/Cleaned datasets")
-
 
 Zhao_metadt <- read.csv('rwl_metadata.csv', header = TRUE)
 Zhao_metadt <- Zhao_metadt[Zhao_metadt$type == 'Ring Width',]
@@ -79,28 +69,28 @@ Zhao_metadt$id <- paste(Zhao_metadt$id, '.crn', sep = '')
 
 setwd('./crn')
 
-list_files <- list.files(path = '.')                                # list of files in Zhao folder, *.crn
+list_files <- list.files(path = '.')                                
 
-length(Zhao_metadt$id)                                              # 4488 files in Zhao csv metadata file, , *.crn
-length(list_files)                                                  # 3888 sites in Zhao folder, *.crn
-length(unique(site_lst$site_lst))                                   # 2105 currently selected files, *.crn
-length(unique(coord$file_name))                                     # 2383 total number of downloaded files, *.crn
-length(unique(Li_etal_sites$site))                                  # 332 sites employed by Li et al, *.crn
+length(Zhao_metadt$id)                                             
+length(list_files)                                                
+length(unique(site_lst$site_lst))                                   
+length(unique(coord$file_name))                                     
+length(unique(Li_etal_sites$site))                                 
 
-length(which((Li_etal_sites$site %in% site_lst$site_lst)))          # 298 sites         of Li etal found in list of 2105
-Li_etal_sites$site[ !(Li_etal_sites$site %in% site_lst$site_lst)]   #  34 sites         of Li etal missing in list of 2105
-Li_etal_sites$site[ !(Li_etal_sites$site %in% coord$file_name)]     # 189 sites         of Li etal missing in coord
+length(which((Li_etal_sites$site %in% site_lst$site_lst)))          
+Li_etal_sites$site[ !(Li_etal_sites$site %in% site_lst$site_lst)]   
+Li_etal_sites$site[ !(Li_etal_sites$site %in% coord$file_name)]     
 
-length(Li_etal_sites$site[Li_etal_sites$site %in% list_files])      # 221 sites         of Li_etal found in Zhao folder
-length(Li_etal_sites$site[Li_etal_sites$site %in% Zhao_metadt$id])  # 320 sites         of Li_etal found in Zhao metadat
-
-
-sites_mssng <- setdiff(Li_etal_sites$site , site_lst$site_lst)      #  34 sites in Lietal, but not in list of 1946
+length(Li_etal_sites$site[Li_etal_sites$site %in% list_files])      
+length(Li_etal_sites$site[Li_etal_sites$site %in% Zhao_metadt$id])  
 
 
-availbl_sites <- intersect(sites_mssng, Zhao_metadt$id)             # 221 sites left are in Zhao metadt: 12 missing
-unavailbl_sites <- sites_mssng[ !(sites_mssng %in% Zhao_metadt$id)] # remaining 12 missing sites   
-setdiff(sites_mssng, Zhao_metadt$id)                                # same result
+sites_mssng <- setdiff(Li_etal_sites$site , site_lst$site_lst)      
+
+
+availbl_sites <- intersect(sites_mssng, Zhao_metadt$id)            
+unavailbl_sites <- sites_mssng[ !(sites_mssng %in% Zhao_metadt$id)]   
+setdiff(sites_mssng, Zhao_metadt$id)                                
 
 
 ## SECOND ROUTINE TO CATCH MISSING FILES___________________####
@@ -122,10 +112,6 @@ for(j in 1:length(list_dirs)) {
   for(site in availbl_sites_rwl) {
     if(site %in% list_files) {
       file <- read.rwl(fname = site, format = 'tucson')
-      # if(!all(unlist(lapply(file, function(x) any(is.na(x) | any(x==0)))))) { # is not all NA and not all 0, then
-      
-      # this option employed the Li et al approach of spline detrending
-      # dtrdn_file <- detrend(file, make.plot = FALSE, method = 'Spline', f = 0.5, nyrs = 30, difference = FALSE)
       
       # this option employed the Gazol et al 2016 approach of Neg Exp detrending
       dtrdn_file <- detrend(file, make.plot = FALSE, method = 'ModNegExp', difference = FALSE)
@@ -139,7 +125,6 @@ for(j in 1:length(list_dirs)) {
         saveRDS(df, file = name)
         setwd('..')                            #  reset to home directory
         setwd(list_dirs[j])                  # reset to same child directory
-        # }
     }
   }
 }
@@ -150,34 +135,33 @@ setwd('..')
 getwd()
 setwd('./crn')
 
-list_files <- list.files(path = '.')                                # list of files in Zhao folder, *.crn
-length(list_files)                                                  # 3889 (3792) sites in Zhao folder, *.crn
-length(Zhao_metadt$id)                                              # 4488 files in Zhao csv metadata file, , *.crn
-length(unique(site_lst$site_lst))                                   # 2119 currently selected files, *.crn
-length(unique(coord$file_name))                                     # 2383 total number of downloaded files, *.crn
-length(unique(Li_etal_sites$site))                                  # 332 sites employed by Li et al, *.crn
+list_files <- list.files(path = '.')                            
+length(list_files)                                                
+length(Zhao_metadt$id)                                              
+length(unique(site_lst$site_lst))                                 
+length(unique(coord$file_name))                                    
+length(unique(Li_etal_sites$site))                                  
 
-length(which((Li_etal_sites$site %in% site_lst$site_lst)))          # 313 sites         of Li etal found in list of 1946
-Li_etal_sites$site[ !(Li_etal_sites$site %in% site_lst$site_lst)]   # 19  sites         of Li etal missing in list of 1946
-Li_etal_sites$site[ !(Li_etal_sites$site %in% coord$file_name)]     # 189 sites         of Li etal missing in coord
+length(which((Li_etal_sites$site %in% site_lst$site_lst)))          
+Li_etal_sites$site[ !(Li_etal_sites$site %in% site_lst$site_lst)]   
+Li_etal_sites$site[ !(Li_etal_sites$site %in% coord$file_name)]   
 
-length(Li_etal_sites$site[Li_etal_sites$site %in% list_files])      # 222(125) sites    of Li_etal found in Zhao folder
-availbl_sites <- Li_etal_sites$site[Li_etal_sites$site %in% list_files] # 222 sites     of Li_etal found in Zhao metadat
+length(Li_etal_sites$site[Li_etal_sites$site %in% list_files])      
+availbl_sites <- Li_etal_sites$site[Li_etal_sites$site %in% list_files] 
 
-length(Li_etal_sites$site[Li_etal_sites$site %in% Zhao_metadt$id])  # 320 sites         of Li_etal found in Zhao metadat
+length(Li_etal_sites$site[Li_etal_sites$site %in% Zhao_metadt$id])  
 
-sites_mssng <- setdiff(Li_etal_sites$site , site_lst$site_lst)      # 19 sites in Lietal, but not in list of 1946
+sites_mssng <- setdiff(Li_etal_sites$site , site_lst$site_lst)    
 
 
-possibl_sites <- intersect(sites_mssng, Zhao_metadt$id)             # 7 sites left are in Zhao metadt: 12 missing
-impossibl_sites <- sites_mssng[ !(sites_mssng %in% Zhao_metadt$id)] # remaining 12 missing sites   
-setdiff(sites_mssng, Zhao_metadt$id)                                # same result
+possibl_sites <- intersect(sites_mssng, Zhao_metadt$id)             
+impossibl_sites <- sites_mssng[ !(sites_mssng %in% Zhao_metadt$id)]   
+setdiff(sites_mssng, Zhao_metadt$id)                             
 
-# crossing available sites from Zhao folder with missing sites in current ilst of 1946
-new_sites <- intersect(sites_mssng, availbl_sites)                  # 7 new sites found (181+139=320) cvd!!!
+# crossing available sites from Zhao folder with missing sites in current list 
+new_sites <- intersect(sites_mssng, availbl_sites)                  
 new_sites
 
-# found all the new files
 
 # create INFOSET_NEW folder with RWI data from only the new sites____________####
 
@@ -198,8 +182,6 @@ for(i in 1:length(new_sites)) {
 }
 
 # create new complete text file of detrended files____________####
-
-# note that list contains not only TRI, but also data for earlywood, latewood, density, etc
 
 setwd('..')
 
